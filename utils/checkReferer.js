6d5c618da01456fefecda2e20346a86b8dfc98e0
ZejusJS <pepitopoky.jp@gmail.com>
2022-12-11 23:33:53 +0100

    lepší reference na /campgrounds_map_data

--- utils/checkReferer.js
+++ utils/checkReferer.js
@@ -6,11 +6,16 @@ module.exports.isFromCampgroundsIndex = function (req, res, next) {
     console.log('Referer.....', req.headers.referer) // adresa ze které se requstuje (undefined pokud to není z js, který je na stránce)
     console.log('req.user.....', req.user)
     const accept = JSON.stringify(req.headers.accept)
-    if (true || req.headers.referer === "https://yelp-camp-a1dk.onrender.com/campgrounds" || req.hostname === "localhost" && req.headers.referer === "http://localhost:3000/campgrounds") {
+    if (req.headers.referer.match(/(https:\/\/yelp-camp-a1dk\.onrender\.com\/campgrounds(?=\?search|\?stype))/g)
+        || req.headers.referer.match(/(https:\/\/yelp-camp-a1dk\.onrender\.com\/campgrounds(?!\/|\?))/g)
+        || req.hostname === "localhost" && req.headers.referer.match(/(http:\/\/localhost:3000\/campgrounds(?=\?search|\?stype))/g)
+        || req.hostname === "localhost" && req.headers.referer.match(/(http:\/\/localhost:3000\/campgrounds(?!\/|\?))/g)) { // || req.hostname === "localhost" && req.headers.referer === "http://localhost:3000/campgrounds"
         if (accept.match(/(application\/json)/g)) {
             return next()
         } else {
             res.redirect('/campgrounds')
         }
+    } else {
+        res.redirect('/campgrounds')
     }
 }
